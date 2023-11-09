31c309540046992a6fdd9c2815c867186a9e9f10
conan0h <conanohara@live.ie>
2023-11-09 19:46:19 +0000

    final

--- wiki/urls.py
+++ wiki/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.get_nearby_wikipedia, name='get_nearby_wikipedia'),
     path('get_location/', views.get_nearby_wikipedia, name='get_nearby_wikipedia'),
 ]
